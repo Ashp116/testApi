3a7cd4dd44177c477bb7f69255057dc8786efd8d
Ashp116 <42877289+Ashp116@users.noreply.github.com>
2023-02-03 23:03:08 -0500

    Update testapi.js

--- src/routes/testapi.js
+++ src/routes/testapi.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/accessories", async (req, res) => {
+const asyncMiddleware = fn => (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+        .catch(next);
+};
+
+router.get("/accessories", asyncMiddleware( async (req, res) => {
     let Cursor = req.query.cursor || ""
     let data = await fetch(`https://catalog.roblox.com/v1/search/items/details?Category=11&Subcategory=19&cursor=${Cursor}`)
     let json = await data.json()
 
-    res.send(json)
-})
+    res.json(json)
+}))
 
 module.exports = router;
