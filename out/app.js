149e63e1dda336a949ae6156df31c43ed6d52679
Ashp116 <42877289+Ashp116@users.noreply.github.com>
2023-02-04 12:17:31 -0500

    out folder ignore

--- out/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const apiTest_1 = require("./routes/apiTest");
-const app = express();
-app.use("/api", apiTest_1.apiRouter);
-module.exports = app;
