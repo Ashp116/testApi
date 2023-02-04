9bb510865bcb22dc6b9f422c4c79b6001209c44e
Ashp116 <42877289+Ashp116@users.noreply.github.com>
2023-02-03 22:47:40 -0500

    Update

---
+++ src/routes/testapi.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/accessories", async (req, res) => {
+    let Cursor = req.query.cursor || ""
+    let data = await fetch(`https://catalog.roblox.com/v1/search/items/details?Category=11&Subcategory=19&cursor=${Cursor}`)
+    let json = await data.json()
+
+    res.send(json)
+})
